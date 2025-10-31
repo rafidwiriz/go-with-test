113c2c068e2954e7d7e2882cc915eb852a42b275
Rafi Dwi <rafidwiriz@gmail.com>
2025-10-31 20:16:32 +0700

    - Add ExampleRepeat - Add param for repeatCount

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,9 +2,7 @@ package iteration
 
 import "strings"
 
-const repeatCount = 5
-
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated strings.Builder
 	for i := 0; i < repeatCount; i++ {
 		repeated.WriteString(character)
